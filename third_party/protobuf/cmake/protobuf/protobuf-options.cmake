9a49839ea859c680924d1b6eef5609684c617662
shiyuan0806 <shiyuan0806@126.com>
2020-02-08 10:23:59 +0800

    first commit large file

---
+++ third_party/protobuf/cmake/protobuf/protobuf-options.cmake
@@ -0,0 +1,7 @@
+# Verbose output
+option(protobuf_VERBOSE "Enable for verbose output" ON)
+mark_as_advanced(protobuf_VERBOSE)
+
+# FindProtobuf module compatibel
+option(protobuf_MODULE_COMPATIBLE "CMake build-in FindProtobuf.cmake module compatible" ON)
+mark_as_advanced(protobuf_MODULE_COMPATIBLE)
